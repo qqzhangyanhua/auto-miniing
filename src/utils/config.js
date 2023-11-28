520e31ca08945af624813779f403996b70b06e40
张艳华 <47852997+qqzhangyanhua@users.noreply.github.com>
2023-11-28 22:09:14 +0800

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,9 +2,9 @@
  * Created by huangqihong on 2022/1/8.
  */
 module.exports = {
-  COOKIE:
-    process.env.COOKIE ||
-    "",
+  COOKIE:`__tea_cookie_tokens_2608=%257B%2522web_id%2522%253A%25227167998419146475048%2522%252C%2522user_unique_id%2522%253A%25227167998419146475048%2522%252C%2522timestamp%2522%253A1668929704573%257D; _ga=GA1.2.1652677588.1668929705; _tea_utm_cache_4366={%22utm_source%22:%221023dhl%22}; _tea_utm_cache_2608={%22utm_source%22:%22course_list%22}; csrf_session_id=5b9681548e8313b73777e2c48e2f5426; passport_csrf_token=8662f48780826a292c395ac604ee7d68; passport_csrf_token_default=8662f48780826a292c395ac604ee7d68; n_mh=nUqqeHABdbQ_GxS4FgpjPCwhPVL4iftLCQ1rLdUjISY; sid_guard=f43ee412d178795ca45d1bcb104e5970%7C1701135972%7C31536000%7CWed%2C+27-Nov-2024+01%3A46%3A12+GMT; uid_tt=b20381249251cdca145117e35f80191b; uid_tt_ss=b20381249251cdca145117e35f80191b; sid_tt=f43ee412d178795ca45d1bcb104e5970; sessionid=f43ee412d178795ca45d1bcb104e5970; sessionid_ss=f43ee412d178795ca45d1bcb104e5970; sid_ucp_v1=1.0.0-KGVmYjhjYmZkY2ExZmJmOTYzZjgwZDBlNjYyYjExOTMyMjkyNTliZmEKFgjImpDA_fXJBBDkjJWrBhiwFDgIQAsaAmxmIiBmNDNlZTQxMmQxNzg3OTVjYTQ1ZDFiY2IxMDRlNTk3MA; ssid_ucp_v1=1.0.0-KGVmYjhjYmZkY2ExZmJmOTYzZjgwZDBlNjYyYjExOTMyMjkyNTliZmEKFgjImpDA_fXJBBDkjJWrBhiwFDgIQAsaAmxmIiBmNDNlZTQxMmQxNzg3OTVjYTQ1ZDFiY2IxMDRlNTk3MA; store-region=cn-sh; store-region-src=uid; _gid=GA1.2.1077335505.1701135974; msToken=JN7XBeogsHk8jbjW3c0Dfqt03KUE9Dm2mE108pHUJd1dS4cczpf0P0C9eFlvZrEyzjolF95C9UAZxqpA9HTwqpDMQmHSVVxY_6g9EPcC4sV2IAfa935-rq5nh5Au4yHP`,
+    // process.env.COOKIE ||
+    // "",
   TOKEN:
     process.env.TOKEN ||
     "",
